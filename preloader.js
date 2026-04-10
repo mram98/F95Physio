6fdc23f6f5def521323b92c903cd690c0c37dc08
mram98 <36890943+mram98@users.noreply.github.com>
2026-04-10 10:12:26 +0000

    update1

--- preloader.js
+++
@@ -1,9 +0,0 @@
-
-  window.addEventListener('load', function() {
-    const loader = document.getElementById('preloader');
-    
-    // Forces a 5-second delay so you can see it
-    setTimeout(() => {
-        loader.classList.add('loader-hidden');
-    }, 2000); 
-  });
